744f96808372497ff507bd7c9095911605f8e159
Muhammed Emin BALCI <102964743+Mebalci@users.noreply.github.com>
2025-08-01 02:28:25 +0300

    Update background_updater.py

--- app/tasks/background_updater.py
+++ app/tasks/background_updater.py
@@ -1,12 +1,12 @@
 import asyncio
-from app.services.trendyol import get_all_products, write_products_to_file
+from app.services.trendyol import get_all_products, kaydet_urunler
 
 async def background_updater():
     while True:
         try:
             print("🔄 Ürün verileri Trendyol'dan güncelleniyor...")
             products = get_all_products()
-            write_products_to_file(products)
+            kaydet_urunler(products)
             print(f"✅ {len(products)} ürün güncellendi ve dosyaya yazıldı.")
         except Exception as e:
             print(f"⚠️ Güncelleme hatası: {e}")
